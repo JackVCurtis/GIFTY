38243cc462713f4656b9d83bf42ace297508b52a
Danny Kulas <dkulas@users.noreply.github.com>
2014-12-12 16:08:56 -0500

    Update 20141212203150_create_user_items.rb

--- db/migrate/20141212203150_create_user_items.rb
+++ db/migrate/20141212203150_create_user_items.rb
@@ -1,6 +1,6 @@
 class CreateUserItems < ActiveRecord::Migration
   def change
-    create_table user_items do |t|
+    create_table :user_items do |t|
       t.integer :user_id
       t.integer :item_id
     end
